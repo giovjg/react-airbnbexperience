91c3ec94ffb10826cb752673fa508eaa0277a23b
giovanna jaramillogutierrez <gjgutierrez@protonmail.com>
2022-07-21 18:02:33 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Navbar component</h1>
+            <img src="../images/airbnb-logo.png"/>
+        </nav>
+    );
+}
