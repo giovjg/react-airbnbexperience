a504a272f5608557f658859025b55761bd0bf2d9
giovanna jaramillogutierrez <gjgutierrez@protonmail.com>
2022-07-22 11:33:30 +0200

    import card to app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,13 @@ import React from "react"
 export default function Card() {
     return (
         <div className="card">
-            <img src="../images/katie-zaferes.png" className="card--image" />
+            <img src="src/images/milanpiscine.png" className="card--image" />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
                 <span>5.0</span>
                 <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span className="gray">Belgium</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
+            <p>Swimming</p>
             <p><span className="bold">From $136</span> / person</p>
         </div>
     )
