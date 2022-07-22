03802976f5745f4799e4bd75b5a90f75c2c6b86c
giovanna jaramillogutierrez <gjgutierrez@protonmail.com>
2022-07-22 11:23:48 +0200

    style text Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,11 @@ export default function Hero() {
     return (
         <section className="hero">
             <img src="/src/images/photo-grid.png" className="hero--photo"/>
+            <div className= "hero-container">
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by
             one-of-a-kind hosts—all without leaving home.</p>
+            </div>
         </section>
     )
 }
